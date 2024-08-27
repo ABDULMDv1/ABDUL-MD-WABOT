951295c86aec1ff870a189a8d5adbc8ab4b8716b
ABD_AZEEZ <169751681+ABDULMDv1@users.noreply.github.com>
2024-08-27 15:53:01 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-onst fs = require('fs');
+const fs = require('fs');
 if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
 
 function convertToBool(text, fault = 'true') {
@@ -6,4 +6,6 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID,
+ALIVE_IMG: process.env.ALIVE_MSG || '> HELLOW I AM ABDUL MD 2.0 WHATSAPP USER BOT',
+ALIVE_MESSAGE: process.env.ALIVE_MESSAGE || 'https://telegra.ph/file/ec513e2adb20e6bb92680.jpg',
 };
