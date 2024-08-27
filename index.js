679bdab8197b6da4d0b8ed8ef116b98064738dac
ABD_AZEEZ <169751681+ABDULMDv1@users.noreply.github.com>
2024-08-27 14:50:09 +0530

    ABDUL MD

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ console.log('Bot connected to whatsapp ✅')
 
 let up = `Abdul MD 2.0 WABOT connected successful ✅\n\nPREFIX: ${prefix}`;
 
-conn.sendMessage(ownerNumber + "@s.whatsapp.net", { image: { url: `https://telegra.ph/file/900435c6d3157c98c3c88.jpg` }, caption: up })
+conn.sendMessage(ownerNumber + "@s.whatsapp.net", { image: { url: `https://telegra.ph/file/ec513e2adb20e6bb92680.jpg` }, caption: up })
 
 }
 })
