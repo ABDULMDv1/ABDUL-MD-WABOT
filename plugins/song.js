cc482275b8fec455b58651cc9dc0cde1947d0a85
ABD_AZEEZ <169751681+ABDULMDv1@users.noreply.github.com>
2024-09-14 20:44:40 +0530

    Create song.js

---
+++ plugins/song.js
@@ -0,0 +1,47 @@
+ const config = require('../config')
+const {cmd , commands} = require('../command')
+const fg = require(`api-dylux`)
+const fg = require(`yt-search`)
+
+
+cmd({
+    pattern: "song",
+    desc: "download songs.",
+    category: "download",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+if(!q) return reply("Please  Give  Me Url  or   Title")
+const search = await yts(q)
+const  data = search.videos[0];
+const url = data.url 
+
+let desc  =  `
+~ABDUL MD SONG DOWNLOAD🎶~
+
+
+*Title: ${data.title}*
+*Descriptoin: ${data.descriptoin}*
+*Time: ${data.timetemp}*
+*Ago: ${data.ago}*
+*Views: ${data.viwes}*
+
+> POWERED BY ABDUL MD 2.0
+`
+await conn.sendMessage(from,{image:{url. data.thumnail},captoin: desc},{quoted:mek});
+
+//download audio
+
+let down = fg.yta(url)
+let downloadUrl = down.dl_url
+
+// send audio message 
+await.conn.sendMessage(from,{audio: {url:downloadUrl },mimetype:"audio/mpeg"},{qutoed:mek})
+
+
+}catch(e){
+ console.log(e)
+reply(`${e}`)    
+}
+})
